19a9a0ab947cec6d3d0c9064389d0bb3cdb2a54f
Tehya Yi <tehyayi@Tehyas-MacBook-Air.local>
2017-01-22 03:33:33 -0800

    games

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/game2', function(req, res, next) {
+  res.render('game2');
+});
+
+router.get('/Game', function(req, res, next) {
+  res.render('Game');
+});
+
+router.get('/game3', function(req, res, next) {
+  res.render('game3');
+});
+
 module.exports = router;
