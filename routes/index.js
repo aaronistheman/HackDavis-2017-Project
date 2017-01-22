bf98824e3d9537ab96073fe80594562d5639d7cd
aaronistheman <aarons.7007@gmail.com>
2017-01-21 18:07:41 -0800

    Junk routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'bob' });
+});
+
+// junk route
+router.get('/dude', function(req, res, next) {
+  res.render('second');
+});
+
+router.get('/main-menu', function(req, res, next) {
+    res.render('main-menu');
 });
 
 module.exports = router;
